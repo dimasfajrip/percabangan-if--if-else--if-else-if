2529cc740eff5a9ae5caa81db27b7f9fa2141b23
dimasfajrip <dimasfp123@gmail.com>
2023-10-03 20:19:45 +0700

    tes1

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	nilai := 80
+
+	// if nilai >= 90 {
+	// 	fmt.Println("Sangat Memuaskan")
+	// } else {
+	// 	fmt.Println("Nilai dibawah 90")
+	// }
+
+	if nilai >= 90 && nilai <= 100 {
+		fmt.Println("Sangat Memuaskan")
+	} else if nilai >= 80 && nilai < 90 {
+		fmt.Println("Memuaskan")
+	} else if nilai >= 70 && nilai  < 80 {
+		fmt.Println("Baik")
+	} else if nilai >= 60 && nilai < 70 {
+		fmt.Println("Cukup")
+	} else if nilai < 60 {
+		fmt.Println("Tidak Lulus")
+	}
+}
